6083e2703593cbaf1166e795b4a0ba7c04612c35
Kaifei Peng <2534164734@qq.com>
2025-05-21 03:46:23 +0000

    New root commit

---
+++ infra/code/network/envManage.go
@@ -0,0 +1,373 @@
+package network
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"syscall"
+	"time"
+	"topo_setup_test/algo"
+
+	"github.com/vishvananda/netlink"
+)
+
+type Server struct {
+	IPAddr             string     `json:"ipAddr"`
+	WorkDir            string     `json:"infraWorkDir"`
+	PhyIntf            string     `json:"phyIntf"`
+	DockerImageName    string     `json:"dockerImageName"`
+	KernFuncsToMonitor [][]string `json:"kernFuncsToMonitor"`
+}
+
+type Servers struct {
+	Servers []Server `json:"servers"`
+}
+
+var (
+	Operation               string
+	ServerList              []Server
+	LocalPhyIntf            string
+	LocalPhyIntfNl          netlink.Link
+	WorkDir                 string
+	TmpDir                  string
+	BinDir                  string
+	CctrBinPath             string
+	CtrLogPath              string
+	LinkLogPath             string
+	LinkLogFile             *os.File
+	KernFuncToolRelPath     string
+	KernFuncLogDir          string
+	CctrMonitorScriptPath   string
+	CctrMonitorOutputPath   string
+	CpuMemMonitorScriptPath string
+	CpuMemMonitorOutputDir  string
+	MonitorCmds             []*exec.Cmd
+	ImageRootfsPath         string
+	Parallel                int
+	DisableIpv6             int
+)
+
+func ConfigServers(confFileName string) error {
+	// Read the JSON file
+	jsonFile, err := os.ReadFile(confFileName)
+	if err != nil {
+		log.Fatalf("Error reading the JSON file: %v", err)
+	}
+
+	var serversData Servers
+
+	// Parse JSON into the struct
+	err = json.Unmarshal(jsonFile, &serversData)
+	if err != nil {
+		return fmt.Errorf("error parsing JSON: %v", err)
+	}
+
+	// Assign the parsed data to the global slice
+	ServerList = serversData.Servers
+	return nil
+}
+
+func ConfigEnvs(serverID int, operation string, disableIpv6 int, parallel int) error {
+	server := ServerList[serverID]
+	Operation = operation
+	setLocalPhyIntf(server.PhyIntf)
+	setEnvPaths(server.WorkDir, server.DockerImageName)
+	setDisableIpv6(disableIpv6)
+	setParallel(parallel)
+	setKernelPtySysctl()
+	prepareRootfs(server.DockerImageName)
+	return nil
+}
+
+func CleanEnvs(operation string) {
+}
+
+func StartMonitor(serverID int, operation string, nmManagerType string) error {
+	var err error
+
+	/* Open link setup log */
+	if operation == "setup" {
+		openLinkLog()
+	}
+
+	/* Start monitoring kernel functions */
+	kernFuncs := ServerList[serverID].KernFuncsToMonitor
+	for _, funcEntry := range kernFuncs {
+		err = startMonitorKernFunc(funcEntry, operation)
+		if err != nil {
+			return err
+		}
+	}
+
+	/* Start monitoring cctr */
+	if operation == "setup" && nmManagerType == "cctr" {
+		err = startMonitorCctr()
+		if err != nil {
+			return err
+		}
+	}
+
+	/* Monitor CPU MEM usage */
+	err = startMonitorCpuMem(operation)
+	if err != nil {
+		return err
+	}
+
+	time.Sleep(2 * time.Second)
+
+	return nil
+}
+
+func StopMonitor(operation string) {
+	/* Close link setup log */
+	if operation == "setup" {
+		closeLinkLog()
+	}
+
+	/* stop monitorcmd */
+	for _, monitorCmd := range MonitorCmds {
+		if monitorCmd != nil && monitorCmd.Process != nil {
+			fmt.Printf("Stopping bpftrace script with PID %d\n", monitorCmd.Process.Pid)
+			if err := monitorCmd.Process.Signal(syscall.SIGTERM); err != nil {
+				fmt.Printf("Error stopping process %d: %v\n", monitorCmd.Process.Pid, err)
+			}
+			monitorCmd.Wait() // Wait for the process to terminate
+		}
+	}
+}
+
+func ArchiveCtrLog(operation string,
+	g *algo.Graph, nodeOrder []int, edgeOrder [][][4]int) error {
+	// var srcLogName string
+	var err error
+	archiveDirPath := CtrLogPath
+
+	// if operation == "setup" {
+	// 	srcLogName = "run.log"
+	// 	err = os.RemoveAll(archiveDirPath)
+	// 	if err != nil {
+	// 		fmt.Printf("Error RemoveAll: %s\n", err)
+	// 		return err
+	// 	}
+	// } else if operation == "clean" {
+	// 	srcLogName = "kill.log"
+	// } else {
+	// 	return fmt.Errorf("invalid operation %s", operation)
+	// }
+
+	/* Create node log archive dir */
+	err = os.MkdirAll(archiveDirPath, os.ModePerm)
+	if err != nil {
+		fmt.Printf("Error MkdirAll: %s\n", err)
+		return err
+	}
+
+	// /* Copy all log */
+	// tmpTime := time.Now()
+	// nodeNum := g.GetNodeNum()
+	// reportTime := 10
+	// nodePerReport := nodeNum / reportTime
+	// for i, nodeId := range nodeOrder {
+	// 	/* Progress reporter */
+	// 	if nodePerReport > 0 && i%nodePerReport == 0 {
+	// 		progress := 100 * i / nodeNum
+	// 		curTime := time.Now()
+	// 		fmt.Printf("%d%% nodes' log are archived, time elapsed from last report: %dms\n", progress, curTime.Sub(tmpTime).Milliseconds())
+	// 		tmpTime = time.Now()
+	// 	}
+
+	// 	/* Copy log */
+	// 	nodeTmpDir := path.Join(TmpDir, "nodes")
+	// 	nodeBaseDirPath := "node" + strconv.Itoa(nodeId)
+	// 	dstLogName := srcLogName + "." + strconv.Itoa(i)
+	// 	srcLogPath := path.Join(nodeTmpDir, nodeBaseDirPath, srcLogName)
+	// 	dstLogPath := path.Join(archiveDirPath, dstLogName)
+	// 	err = copyFile(srcLogPath, dstLogPath)
+	// 	if err != nil {
+	// 		fmt.Printf("Error copyFile: %s\n", err)
+	// 		return err
+	// 	}
+	// }
+
+	return nil
+}
+
+func setLocalPhyIntf(value string) {
+	LocalPhyIntf = value
+	LocalPhyIntfNl, _ = netlink.LinkByName(LocalPhyIntf)
+}
+
+func setEnvPaths(workDir string, dockerImageName string) {
+	WorkDir = workDir
+	TmpDir = path.Join(WorkDir, "tmp")
+	BinDir = path.Join(WorkDir, "bin")
+	CctrBinPath = path.Join(BinDir, "cctr")
+	CtrLogPath = path.Join(TmpDir, "ctr_log")
+	LinkLogPath = path.Join(TmpDir, "link_log.txt")
+	KernFuncToolRelPath = path.Join(WorkDir, "scripts", "monitor_kern_func.sh")
+	KernFuncLogDir = path.Join(TmpDir, "kern_func")
+	CctrMonitorScriptPath = path.Join(WorkDir, "scripts", "monitor_cctr_time.sh")
+	CctrMonitorOutputPath = path.Join(TmpDir, "cctr_time.txt")
+	CpuMemMonitorScriptPath = path.Join(WorkDir, "scripts", "monitor_cpu_mem_usage.py")
+	CpuMemMonitorOutputDir = TmpDir
+
+	splitedImageName := strings.Split(dockerImageName, ":")
+	ImageRepo := splitedImageName[0]
+	ImageTag := "latest"
+	if len(splitedImageName) > 1 {
+		ImageTag = splitedImageName[1]
+	}
+	ImageRootfsPath = path.Join(TmpDir, "img_bundles", ImageRepo, ImageTag, "rootfs")
+}
+
+func setDisableIpv6(disableIpv6 int) {
+	DisableIpv6 = disableIpv6
+}
+
+func setParallel(parallel int) {
+	Parallel = parallel
+}
+
+func setSysctlValue(path string, value string) error {
+	file, err := os.OpenFile(path, os.O_WRONLY, 0644)
+	if err != nil {
+		return fmt.Errorf("failed to open file %s: %w", path, err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString(value)
+	if err != nil {
+		return fmt.Errorf("failed to write value to file %s: %w", path, err)
+	}
+
+	return nil
+}
+
+func setKernelPtySysctl() {
+	ptyMaxPath := "/proc/sys/kernel/pty/max"
+	ptyReservePath := "/proc/sys/kernel/pty/reserve"
+
+	// Desired values
+	newMaxValue := "262144"
+	newReserveValue := "65536"
+
+	if err := setSysctlValue(ptyMaxPath, newMaxValue); err != nil {
+		log.Fatalf("Error setting kernel.pty.max: %v", err)
+	} else {
+		fmt.Printf("Successfully set kernel.pty.max to %s\n", newMaxValue)
+	}
+
+	// Modify kernel.pty.reserve
+	if err := setSysctlValue(ptyReservePath, newReserveValue); err != nil {
+		log.Fatalf("Error setting kernel.pty.reserve: %v", err)
+	} else {
+		fmt.Printf("Successfully set kernel.pty.reserve to %s\n", newReserveValue)
+	}
+}
+
+func prepareRootfs(dockerImageName string) {
+	splitedImageName := strings.Split(dockerImageName, ":")
+	ImageRepo := splitedImageName[0]
+	ImageTag := "latest"
+	if len(splitedImageName) > 1 {
+		ImageTag = splitedImageName[1]
+	}
+	dockerImageName = ImageRepo + ":" + ImageTag
+
+	prepareScriptPath := path.Join(WorkDir, "scripts", "prepare_rootfs.sh")
+	fmt.Printf("prepareScriptPath: %s\n", prepareScriptPath)
+	fmt.Printf("dockerImageName: %s\n", dockerImageName)
+	prepareCommand := exec.Command(
+		"bash", prepareScriptPath, dockerImageName)
+	prepareCommand.Run()
+}
+
+func openLinkLog() error {
+	var err error
+	LinkLogFile, err = os.OpenFile(LinkLogPath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0644)
+	if err != nil {
+		LinkLogFile.Close()
+		return fmt.Errorf("failed to open link log file: %v", err)
+	}
+	return nil
+}
+
+func closeLinkLog() {
+	LinkLogFile.Close()
+}
+
+func copyFile(src, dst string) error {
+	sourceFile, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("error opening source file: %w", err)
+	}
+	defer sourceFile.Close()
+
+	destinationFile, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("error creating destination file: %w", err)
+	}
+	defer destinationFile.Close()
+
+	_, err = io.Copy(destinationFile, sourceFile)
+	if err != nil {
+		return fmt.Errorf("error copying file contents: %w", err)
+	}
+
+	err = destinationFile.Sync()
+	if err != nil {
+		return fmt.Errorf("error syncing destination file: %w", err)
+	}
+
+	return nil
+}
+
+func startMonitorKernFunc(funcEntry []string, operation string) error {
+	op := funcEntry[0]
+	if op != operation {
+		return nil
+	}
+	comm := funcEntry[1]
+	kernFunc := funcEntry[2]
+	outputFileName := fmt.Sprintf("%s--%s.txt", comm, kernFunc)
+	outputFilePath := path.Join(KernFuncLogDir, outputFileName)
+	monitorCmd := exec.Command(KernFuncToolRelPath, comm, kernFunc, outputFilePath)
+
+	//start monitorcmd
+	if err := monitorCmd.Start(); err != nil {
+		return fmt.Errorf("error starting bpftrace: %v", err)
+	}
+	fmt.Printf("Started kernel function monitoring with PID %d\n", monitorCmd.Process.Pid)
+
+	MonitorCmds = append(MonitorCmds, monitorCmd)
+	return nil
+}
+
+func startMonitorCctr() error {
+	monitorCmd := exec.Command(CctrMonitorScriptPath, CctrMonitorOutputPath)
+	if err := monitorCmd.Start(); err != nil {
+		return fmt.Errorf("error starting bpftrace: %v", err)
+	}
+	fmt.Printf("Started cctr monitoring with PID %d\n", monitorCmd.Process.Pid)
+
+	MonitorCmds = append(MonitorCmds, monitorCmd)
+	return nil
+}
+
+func startMonitorCpuMem(operation string) error {
+	CpuMemMonitorOutputPath := path.Join(CpuMemMonitorOutputDir, operation+"_cpu_mem_usage.txt")
+	monitorCmd := exec.Command(
+		"python3", "-u", CpuMemMonitorScriptPath, CpuMemMonitorOutputPath)
+	if err := monitorCmd.Start(); err != nil {
+		return fmt.Errorf("error starting bpftrace: %v", err)
+	}
+	fmt.Printf("Started cctr monitoring with PID %d\n", monitorCmd.Process.Pid)
+
+	MonitorCmds = append(MonitorCmds, monitorCmd)
+	return nil
+}
---
+++ infra/code/network/ntlBrManager.go
@@ -0,0 +1,429 @@
+package network
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"os/exec"
+	"strconv"
+	"sync"
+
+	"github.com/vishvananda/netlink"
+	"github.com/vishvananda/netns"
+)
+
+type NtlBrLinkManager struct {
+	curBackBoneNum  int
+	curlinkNum      int
+	curlinkNumMutex sync.Mutex
+	curBackBoneNs   netns.NsHandle
+	hostNetns       netns.NsHandle
+	nm              NodeManager
+}
+
+func (lm *NtlBrLinkManager) Init(nm NodeManager) error {
+	var err error
+
+	lm.curBackBoneNum = 0
+	lm.curlinkNum = 0
+	lm.nm = nm
+	lm.curBackBoneNs = -1
+	lm.hostNetns, err = netns.Get()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (lm *NtlBrLinkManager) Delete() error {
+	lm.hostNetns.Close()
+	return nil
+}
+
+func (lm *NtlBrLinkManager) SetupBbNs() (netns.NsHandle, error) {
+	bbnsName := "bbns" + strconv.Itoa(lm.curBackBoneNum)
+	backboneNsHandle, err := netns.NewNamed(bbnsName)
+	if err != nil {
+		fmt.Printf("failed to netns.NewNamed %s: %s\n", bbnsName, err)
+		return -1, err
+	}
+	lm.curBackBoneNum += 1
+	return backboneNsHandle, nil
+}
+
+func (lm *NtlBrLinkManager) EnterBbNs(bbnsIndex int) (netns.NsHandle, error) {
+	bbnsName := "bbns" + strconv.Itoa(bbnsIndex)
+	backboneNsHandle, err := netns.GetFromName(bbnsName)
+	if err != nil {
+		fmt.Printf("failed to netns.NewNamed %s: %s\n", bbnsName, err)
+		return -1, err
+	}
+
+	err = netns.Set(backboneNsHandle)
+	if err != nil {
+		fmt.Printf("failed to netns.Set for bbns: %s\n", err)
+		return -1, err
+	}
+
+	if DisableIpv6 == 1 {
+		err = disableIpv6ForCurNetns()
+		if err != nil {
+			fmt.Printf("failed to DisableIpv6 for current Netns: %s\n", err)
+			return -1, err
+		}
+	}
+
+	if lm.curBackBoneNs != -1 {
+		lm.curBackBoneNs.Close()
+	}
+	lm.curBackBoneNs = backboneNsHandle
+	return backboneNsHandle, nil
+}
+
+func (lm *NtlBrLinkManager) CleanAllBbNs() error {
+	// var origNs, bbnsHandle netns.NsHandle
+	// var err error
+
+	// origNs, err = netns.Get()
+	// if err != nil {
+	// 	fmt.Printf("failed to netns.Get of origNs: %s\n", err)
+	// 	return err
+	// }
+
+	// /* Disable bbns ipv6 */
+	// bbnsNames := getAllBbNs()
+	// for _, bbnsName := range bbnsNames {
+	// 	bbnsHandle, err = netns.GetFromName(bbnsName)
+	// 	if err != nil {
+	// 		return fmt.Errorf("error when getting bbns handle, %v", err)
+	// 	}
+	// 	fmt.Printf("Disable ipv6 for bbns: %s\n", bbnsName)
+	// 	err = netns.Set(bbnsHandle)
+	// 	if err != nil {
+	// 		fmt.Printf("failed to netns.Set to bbns: %s\n", err)
+	// 		return err
+	// 	}
+	// 	err = disableIpv6ForCurNetns()
+	// 	if err != nil {
+	// 		fmt.Printf("failed to disableIpv6ForCurNetns for bbns: %s\n", err)
+	// 		return err
+	// 	}
+	// }
+	// err = netns.Set(origNs)
+	// if err != nil {
+	// 	fmt.Printf("failed to netns.Set to origNs: %s\n", err)
+	// 	return err
+	// }
+
+	/* Destroy all netns */
+	destroyCommand := exec.Command(
+		"ip", "-all", "netns", "del")
+	destroyCommand.Stdout = os.Stdout
+	destroyCommand.Run()
+	return nil
+}
+
+func (lm *NtlBrLinkManager) CleanAllLinks(groupID int) error {
+	var origNs, bbnsHandle netns.NsHandle
+	var err error
+
+	origNs, err = netns.Get()
+	if err != nil {
+		fmt.Printf("failed to netns.Get of origNs: %s\n", err)
+		return err
+	}
+
+	/* Clean links in all bbns'es */
+	bbnsNames := getAllBbNs()
+	for _, bbnsName := range bbnsNames {
+		bbnsHandle, err = netns.GetFromName(bbnsName)
+		if err != nil {
+			return fmt.Errorf("error when getting bbns handle, %v", err)
+		}
+		err = netns.Set(bbnsHandle)
+		if err != nil {
+			fmt.Printf("failed to netns.Set to bbns: %s\n", err)
+			return err
+		}
+		delCommand := exec.Command(
+			"ip", "link", "del", "group", strconv.Itoa(groupID))
+		delCommand.Run()
+	}
+
+	err = netns.Set(origNs)
+	if err != nil {
+		fmt.Printf("failed to netns.Set to origNs: %s\n", err)
+		return err
+	}
+
+	return nil
+}
+
+/* Before calling SetupLink, current network namespace must be backbone namespace */
+func (lm *NtlBrLinkManager) SetupLink(nodeIdi int, nodeIdj int, serverID int, vxlanID int, groupID int) error {
+	var err error
+	if vxlanID == -1 {
+		err = lm.SetupInternalLink(nodeIdi, nodeIdj, serverID, vxlanID, groupID)
+	} else {
+		err = lm.SetupExternalLink(nodeIdi, nodeIdj, serverID, vxlanID, groupID)
+	}
+	return err
+}
+
+/* Before calling SetupInternalLink, current network namespace must be backbone namespace */
+func (lm *NtlBrLinkManager) SetupInternalLink(nodeIdi int, nodeIdj int, serverID int, vxlanID int, groupID int) error {
+	var err error
+	var backboneNs, nodeiNetNs, nodejNetNs netns.NsHandle
+	var brName string
+
+	/* Prepare network namespace handles */
+	backboneNs = lm.curBackBoneNs
+	nodeiNetNs, err = lm.nm.GetNodeNetNs(nodeIdi)
+	if err != nil {
+		return fmt.Errorf("failed to GetNodeNetNs: %s", err)
+	}
+	defer nodeiNetNs.Close()
+	nodejNetNs, err = lm.nm.GetNodeNetNs(nodeIdj)
+	if err != nil {
+		return fmt.Errorf("failed to GetNodeNetNs: %s", err)
+	}
+	defer nodejNetNs.Close()
+
+	/* Prepare other data structure */
+	if Parallel > 0 {
+		lm.curlinkNumMutex.Lock()
+	}
+	lm.curlinkNum += 1
+	brName = "br-" + strconv.Itoa(lm.curlinkNum)
+	vethNamei := "eth-" + strconv.Itoa(lm.curlinkNum) + "-i"
+	vethNamej := "eth-" + strconv.Itoa(lm.curlinkNum) + "-j"
+	if Parallel > 0 {
+		lm.curlinkNumMutex.Unlock()
+	}
+	br := &netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  brName,
+			MTU:   1450,
+			Flags: net.FlagUp,
+			Group: uint32(groupID),
+		},
+	}
+	vethOuti := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vethNamei,
+			MTU:   1450,
+			Flags: net.FlagUp,
+			Group: uint32(groupID),
+			// MasterIndex: br.Index,
+		},
+		PeerName:      vethNamei,
+		PeerNamespace: netlink.NsFd(nodeiNetNs),
+	}
+	vethOutj := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vethNamej,
+			MTU:   1450,
+			Flags: net.FlagUp,
+			Group: uint32(groupID),
+			// MasterIndex: br.Index,
+		},
+		PeerName:      vethNamej,
+		PeerNamespace: netlink.NsFd(nodejNetNs),
+	}
+	vethIni := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vethNamei,
+			Group: uint32(groupID),
+		},
+	}
+	vethInj := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vethNamej,
+			Group: uint32(groupID),
+		},
+	}
+
+	/* Create a bridge and two veth pairs */
+	if err := netlink.LinkAdd(br); err != nil {
+		return fmt.Errorf("failed to create bridge: %s", err)
+	}
+	vethOuti.Attrs().MasterIndex = br.Index
+	err = netlink.LinkAdd(vethOuti)
+	if err != nil {
+		return fmt.Errorf("failed to create veth: %s", err)
+	}
+	vethOutj.Attrs().MasterIndex = br.Index
+	err = netlink.LinkAdd(vethOutj)
+	if err != nil {
+		return fmt.Errorf("failed to create veth: %s", err)
+	}
+
+	/* Set the other sides of veths up */
+	err = netns.Set(nodeiNetNs)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	err = netlink.LinkSetUp(vethIni)
+	if err != nil {
+		return fmt.Errorf("failed to LinkSetUp: %s", err)
+	}
+	err = netns.Set(nodejNetNs)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	err = netlink.LinkSetUp(vethInj)
+	if err != nil {
+		return fmt.Errorf("failed to LinkSetUp: %s", err)
+	}
+
+	/* Set NetNs Back */
+	err = netns.Set(backboneNs)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	return err
+}
+
+/* Before calling SetupExternalLink, current network namespace must be backbone namespace */
+func (lm *NtlBrLinkManager) SetupExternalLink(nodeIdi int, nodeIdj int, serverID int, vxlanID int, groupID int) error {
+	var err error
+	var backboneNs, nodeiNetNs netns.NsHandle
+
+	/* Prepare network namespace handles */
+	backboneNs = lm.curBackBoneNs
+	nodeiNetNs, err = lm.nm.GetNodeNetNs(nodeIdi)
+	if err != nil {
+		return fmt.Errorf("failed to GetNodeNetNs: %s", err)
+	}
+	defer nodeiNetNs.Close()
+
+	/* Create Vxlan */
+	if Parallel > 0 {
+		lm.curlinkNumMutex.Lock()
+	}
+	brName := "br-" + strconv.Itoa(lm.curlinkNum)
+	vxlanName := "eth-" + strconv.Itoa(lm.curlinkNum) + "-v"
+	vethNamei := "vxl-" + strconv.Itoa(lm.curlinkNum)
+	if Parallel > 0 {
+		lm.curlinkNumMutex.Unlock()
+	}
+	br := &netlink.Bridge{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  brName,
+			MTU:   1450,
+			Flags: net.FlagUp,
+			Group: uint32(groupID),
+		},
+	}
+	vxlanOut := &netlink.Vxlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vxlanName,
+			Group: uint32(groupID),
+		},
+		VxlanId:      vxlanID,
+		VtepDevIndex: LocalPhyIntfNl.Attrs().Index,
+		Port:         4789,
+		Group:        net.ParseIP(ServerList[serverID].IPAddr),
+		Learning:     true,
+	}
+	vxlanIn := &netlink.Vxlan{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vxlanName,
+			Group: uint32(groupID),
+		},
+	}
+	vethOuti := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vethNamei,
+			MTU:   1450,
+			Flags: net.FlagUp,
+			Group: uint32(groupID),
+			// MasterIndex: br.Index,
+		},
+		PeerName:      vethNamei,
+		PeerNamespace: netlink.NsFd(nodeiNetNs),
+	}
+	vethIni := &netlink.Veth{
+		LinkAttrs: netlink.LinkAttrs{
+			Name:  vethNamei,
+			Group: uint32(groupID),
+		},
+	}
+
+	/* Create network devices */
+	err = netns.Set(lm.hostNetns)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	if err = netlink.LinkAdd(vxlanOut); err != nil {
+		return fmt.Errorf("failed to create vxlan interface: %s", err)
+	}
+	err = netlink.LinkSetNsFd(vxlanOut, int(backboneNs))
+	if err != nil {
+		return fmt.Errorf("failed to link set nsfd: %s", err)
+	}
+	err = netns.Set(backboneNs)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	if err := netlink.LinkAdd(br); err != nil {
+		return fmt.Errorf("failed to create bridge: %s", err)
+	}
+	vethOuti.Attrs().MasterIndex = br.Index
+	err = netlink.LinkAdd(vethOuti)
+	if err != nil {
+		return fmt.Errorf("failed to create VethPeer in nodeiNetNs: %s", err)
+	}
+
+	/* Set Vxlan master and set Vxlan up */
+	var newVxlan netlink.Link
+	err = netlink.LinkSetMaster(vxlanIn, br)
+	if err != nil {
+		return fmt.Errorf("failed to LinkSetMaster (%d, %d, %d, %s, %v): %s", nodeIdi, nodeIdj, vxlanID, brName, br, err)
+	}
+	err = netlink.LinkSetUp(newVxlan)
+	if err != nil {
+		return fmt.Errorf("failed to LinkSetUp: %s", err)
+	}
+	err = netns.Set(nodeiNetNs)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	err = netlink.LinkSetUp(vethIni)
+	if err != nil {
+		return fmt.Errorf("failed to LinkSetUp: %s", err)
+	}
+
+	/* Set NetNs Back */
+	err = netns.Set(backboneNs)
+	if err != nil {
+		return fmt.Errorf("failed to netns.Set: %s", err)
+	}
+	return err
+}
+
+func getAllBbNs() []string {
+	// Directory where ip netns namespaces are stored
+	netnsDir := "/var/run/netns"
+
+	// Read the contents of the netns directory
+	files, err := ioutil.ReadDir(netnsDir)
+	if err != nil {
+		if os.IsNotExist(err) {
+			log.Fatalf("Network namespace directory does not exist: %v", err)
+		}
+		log.Fatalf("Error reading %s: %v", netnsDir, err)
+	}
+
+	// Collect namespace names
+	var namespaces []string
+	for _, file := range files {
+		if !file.IsDir() {
+			namespaces = append(namespaces, file.Name())
+		}
+	}
+
+	return namespaces
+}
